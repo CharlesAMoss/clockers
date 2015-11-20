c5a60c30880d0e85c01c3f55e00eaf64fd29ffd1
Charles Moss <student@epicodus.com>
2015-11-20 14:30:46 -0800

    add ternary operator

--- js/clock.js
+++ js/clock.js
@@ -1,35 +1,33 @@
 function updateClock(){
-	
-	//pulls in curret time 
+
+	//pulls in curret time
 	var now = new Date();
-	
+
 	//breaks out hour, minutes, and seconds form curret time
 	var hours = now.getHours();
 	var minutes = now.getMinutes();
 	var seconds = now.getSeconds();
-	
-	//formating 
+
+	// afternoon? am or pm
+	var meridiem = hours > 12 ? "pm" : "am";
+
+	//formating
 	if (hours > 12) {
 		hours = hours - 12;
-		var meridiem = "pm";
-	} else {
-		var meridiem = "am";
 	}
-	
-	
 	if (hours < 10) {
-		hours = "0" + hours;
-	} 
+		hours = "&nbsp;" + hours;
+	}
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
-	if (seconds < 10) { 
+	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	
+
 	//gets element
 	var elem = document.getElementById('clock');
-	
+
 	//sets the display of content
 	elem.innerHTML = hours + ':' + minutes + ':' + seconds + ' ' + meridiem;
 }
